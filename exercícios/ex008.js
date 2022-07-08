0776263d2edde6272a8a02fb231e002f25f06e1e
chasmalin <107214199+chasmalin@users.noreply.github.com>
2022-07-08 08:54:10 -0400

    Create ex008.js

---
+++ exercícios/ex008.js
@@ -0,0 +1,7 @@
+console.log('o console tá funcionando, yay!');
+var vel = 70;
+console.log(`A VELOCIDADE DO SEU CARRO É ${vel}Km/h`);
+console.log('use sempre o CINTO DE SEGURANÇA!!!');
+if(vel > 60){
+    console.log('limite de velocidade ultrapassado, você foi MULTADO!')
+}
