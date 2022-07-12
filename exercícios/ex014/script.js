1f13c8051908bff587cd3d51cc89359b38bf6b6a
chasmalin <107214199+chasmalin@users.noreply.github.com>
2022-07-12 16:34:24 -0400

    edit fotos

--- exercícios/ex014/script.js
+++ exercícios/ex014/script.js
@@ -1,1 +1,19 @@
 //alert('olá');
+function carregar(){
+    var msg = window.document.getElementById('msg');
+    var img = window.document.getElementById('imagem');
+    var data = new Date();
+    var hora = data.getHours();
+    var min = data.getMinutes();
+    msg.innerHTML = `Agora são ${hora} horas e ${min} minutos. (=`;
+    if (hora >= 0 && hora < 12){
+        //bom dia
+        img.src = '../aula12_exercicios/imagens/amanha.png';
+    } else if (hora >= 12 && hora < 18){
+        //boa tarde
+        img.src = '../aula12_exercicios/imagens/atarde.png';
+    } else{
+        //boa noite
+        img.src = '../aula12_exercicios/imagens/anoite.png';
+    }
+}
