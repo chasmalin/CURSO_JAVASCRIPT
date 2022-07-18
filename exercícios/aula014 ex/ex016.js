5a1f2cec235248e4d39a0d39f41d8822972d59f4
chasmalin <107214199+chasmalin@users.noreply.github.com>
2022-07-18 16:46:41 -0400

    passo

--- exercícios/aula014 ex/ex016.js
+++ exercícios/aula014 ex/ex016.js
@@ -8,13 +8,25 @@ function contar(){
     if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
         window.alert('[ERRO] vazio!');
     } else {
-        res.innerHTML = 'Contando...';
+        res.innerHTML = 'Contando... ';
         let i = Number(ini.value);
         let f = Number(fim.value);
         let p = Number(passo.value);
 
-        for(let c = i; c <= f; c += p){
-            res.innerHTML+= `${c} `;
+        if (p <= 0){
+            window.alert('Passo inválido! Vou considerar como passo 1!');
+            p = 1;
         }
+
+        if (i < f){
+            for(let c = i; c <= f; c += p){
+                res.innerHTML+= `${c} \u{1F449} `;
+            }
+        } else{
+            for(let c = i; c >= f; c -= p){
+                res.innerHTML+= `${c} \u{1F449} `;
+            }
+        }
+        res.innerHTML += ` \u{1F4A9}`;
     }
 }
