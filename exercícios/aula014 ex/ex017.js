3442c8e8d634bd219690f078de061e1d967299f6
chasmalin <107214199+chasmalin@users.noreply.github.com>
2022-07-18 17:17:51 -0400

    tabuada

--- exercícios/aula014 ex/ex017.js
+++ exercícios/aula014 ex/ex017.js
@@ -1,10 +1,18 @@
 function tabuada(){
     let num = document.getElementById('txtn');
     let tab = document.getElementById('seltab');
-    window.alert('Digite um número!');
     if (num.value.length == 0) {
         window.alert('Digite um número!');
     } else {
         let n = Number(num.value);
+        let c = 1;
+        tab.innerHTML = '';
+        while (c <= 10){
+            let item = document.createElement('option');
+            item.text = `${n} x ${c} = ${n*c}`;
+            item.value = `tab${c}`; // dando nomes aos options
+            tab.appendChild(item); // acho que essa acrescenta cada linha
+            c++;
+        }
     }
 }
