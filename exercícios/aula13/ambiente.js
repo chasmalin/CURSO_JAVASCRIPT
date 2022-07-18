72013e16cbb9b290ac78b9acf5b19b995f180e44
chasmalin <107214199+chasmalin@users.noreply.github.com>
2022-07-18 15:20:08 -0400

    for

--- exercícios/aula13/ambiente.js
+++ exercícios/aula13/ambiente.js
@@ -4,8 +4,13 @@ var c = 1;
     console.log(`turupom? [${c}]`);
     c++;
 }
- */
+ 
 do{
     console.log(`turupom? [${c}]`);
     c++;
 } while (c <= 4);
+*/
+
+for(var d=1; d<=10; d++){
+    console.log(`turupom? [${d}]`);
+}
